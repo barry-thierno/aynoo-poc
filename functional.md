ec19cafcb5e36adb661684655503098a80b7381b
barry-thierno <thierno.barry.partner@decathlon.com>
2023-12-30 12:32:48 +0100

    feat: add heat map

--- functional.md
+++ functional.md
@@ -6,4 +6,7 @@ On souscrit à l'alerting par mail de la NASA. Sur chaque alerte on notifie tous
 
 ## Utilisation des données sur les 10 derniers jours
 
+The end point for NASA
+https://firms.modaps.eosdis.nasa.gov/api/country/csv/205dd6cbd76937af48043319cca57731/VIIRS_SNPP_NRT/GIN/10/2023-12-29
+
 ## Utilisation des données des 5 à 10 dernières années
--- map.js
+++ map.js
@@ -1,56 +1,95 @@
-const map = L.map("map").setView([10.046285, -13.796768], 15);
+import fireData from "./ressources/fire_data.json" assert { type: "json" };
+
+// const map = L.map("map").setView([10.046285, -13.796768], 15);
+
+const greenIcon = L.icon({
+  iconUrl: "./ressources/icons/icons8-user-location-100.png",
+  // shadowUrl: 'leaf-shadow.png',
+
+  iconSize: [40, 40], // size of the icon
+  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62], // the same for the shadow
+  popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+});
+// Add markers
+const waranya = L.marker([10.046285, -13.796768], {
+  icon: greenIcon,
+}).bindPopup("Domaine agricole de Mr et Mm BARRY");
+
+const cropFields = L.layerGroup([waranya]);
+
+const fields = {
+  CropFields: cropFields,
+};
+
+// Map tiles
+const map = L.map("map").setView([10.268979, -10.977505], 7);
 
 const openStreetMap = L.tileLayer(
-    "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-    {
-        maxZoom: 19,
-        attribution:
-            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }
+  "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+  {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }
 );
 
 const waterColor = L.tileLayer.provider("Stadia.StamenWatercolor");
 
 const googleSat = L.tileLayer(
-    "http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
-    {
-        maxZoom: 20,
-        subdomains: ["mt0", "mt1", "mt2", "mt3"],
-    }
+  "http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
+  {
+    maxZoom: 20,
+    subdomains: ["mt0", "mt1", "mt2", "mt3"],
+  }
 );
-googleSat.addTo(map);
 
 const googleTerrain = L.tileLayer(
-    "http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}",
-    {
-        maxZoom: 20,
-        subdomains: ["mt0", "mt1", "mt2", "mt3"],
-    }
+  "http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}",
+  {
+    maxZoom: 20,
+    subdomains: ["mt0", "mt1", "mt2", "mt3"],
+  }
 );
 
 const esriSatelite = L.tileLayer.provider("Esri.WorldImagery", {
-    maxZoom: 20,
+  maxZoom: 20,
 });
 
+const stadiaDark = L.tileLayer.provider("Stadia.AlidadeSmoothDark", {
+  maxZoom: 20,
+  minZoom: 7.5,
+  opacity: 0.5,
+});
+
+// const jawgDark = L.tileLayer.provider("Jawg.Dark", {
+//     maxZoom: 20,
+// });
+
+// googleSat.addTo(map);
+// openStreetMap.addTo(map);
+stadiaDark.addTo(map);
+
 const baseMaps = {
-    EsriSatelite: esriSatelite,
-    OpenStreetMap: openStreetMap,
-    Water: waterColor,
-    GoogleTerrain: googleTerrain,
-    GoogleSat: googleSat,
+  EsriSatelite: esriSatelite,
+  OpenStreetMap: openStreetMap,
+  Water: waterColor,
+  GoogleTerrain: googleTerrain,
+  GoogleSat: googleSat,
+  StadiaDark: stadiaDark,
+  // JawgDark: jawgDark
 };
-L.control.layers(baseMaps).addTo(map);
+L.control.layers(baseMaps, fields).addTo(map);
 
 // leaflet draw
-
 const drawControl = new L.Control.Draw({
-    draw: {
-        polygon: {
-            shapeOptions: {
-                color: "pink",
-            },
-        },
+  draw: {
+    polygon: {
+      shapeOptions: {
+        color: "pink",
+      },
     },
+  },
 });
 map.addControl(drawControl);
 
@@ -58,10 +97,18 @@ const drawFeature = new L.FeatureGroup();
 map.addLayer(drawFeature);
 
 map.on("draw:created", (event) => {
-    const { layer } = event;
-    console.log(layer.toGeoJSON());
-    drawFeature.addLayer(layer);
+  const { layer } = event;
+  console.log(layer.toGeoJSON());
+  drawFeature.addLayer(layer);
 });
 
-// NASA End Point
-// https://firms.modaps.eosdis.nasa.gov/api/country/csv/205dd6cbd76937af48043319cca57731/VIIRS_SNPP_NRT/GIN/10/2023-12-29
+// Load NASA fire point for Guinea
+const heatPoints = fireData.map((fd) => [
+  fd.latitude,
+  fd.longitude,
+  fd.bright_ti4,
+]);
+var heat = L.heatLayer(heatPoints, {
+  radius: 25,
+  //   gradient: { 0.4: "#f8961e", 0.65: "#6930c3", 1: "#f94144" },
+}).addTo(map);
